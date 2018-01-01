94dc661369daacc19b44aa90dd990effd1feb3a4
Atul kumar singh <atul19251@gmail.com>
2018-01-02 00:56:29 +0530

    phone book upload

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Phonebook</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"></link>
+  </head>
+  <body>
+   <div id="app">
+     <My-header>
+      
+
+    </My-header>
+   </div>
+   <scripts src="{{ asset('js/app.js') }}">
+
+   </scripts>
+  </body>
+</html>
